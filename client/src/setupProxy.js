c3c8818f4cbae088e3928881e4167481d2e3804d
Piotr Hajder <phajder@agh.edu.pl>
2024-04-19 18:42:23 +0200

    Added onError message to custom proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -9,6 +9,25 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: `${proxyHost}${apiPrefix}`,
       changeOrigin: true,
+      on: {
+        proxyReq: (proxyReq) => {
+          // Browsers may send Origin headers even with same-origin
+          // requests. To prevent CORS issues, we have to change
+          // the Origin to match the target URL.
+          if (proxyReq.getHeader("origin")) {
+            proxyReq.setHeader("origin", proxyHost);
+          }
+        },
+        error: (err, req, res) => {
+          const host = req.headers && req.headers.host;
+          if (res.writeHead && !res.headersSent) {
+            res.writeHead(500);
+          }
+          res.end(
+            `Proxy error: Could not proxy request ${apiPrefix}${req.url} from ${host} to ${proxyHost} (${err.code}).`,
+          );
+        },
+      },
     }),
   );
 };
